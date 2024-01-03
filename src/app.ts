ab6091b7fe867d0f57df8944e80d3969b1e2a0d1
André Luiz <silva.ala82@gmail.com>
2024-01-03 18:44:39 -0300

    feat: implements global handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation Error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: should log to external tool of observability like DataDog/newRelic
+  }
+
+  return reply.status(500).send({ message: "Internal Server Error." });
+});
