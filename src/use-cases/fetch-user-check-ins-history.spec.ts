45f3993c854578541e1135e45fa3ebd14dedc9de
André Luiz <silva.ala82@gmail.com>
2024-01-04 14:26:37 -0300

    test: ensures that should be ble to fetch users check-in history and paginated

---
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,58 @@
+import { expect, describe, it } from "vitest";
+import { beforeEach } from "vitest";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+
+import { FetchUserCheckInHistoryUseCase } from "./fetch-user-check-ins-history";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: FetchUserCheckInHistoryUseCase;
+
+describe("Fetch user check-in history use case", () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new FetchUserCheckInHistoryUseCase(checkInsRepository);
+  });
+
+  it("Should  be able to fetch check-in history", async () => {
+    await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    await checkInsRepository.create({
+      gym_id: "gym-02",
+      user_id: "user-01",
+    });
+
+    const { checkIns } = await sut.execute({
+      userId: "user-01",
+      page: 1,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: "gym-01" }),
+      expect.objectContaining({ gym_id: "gym-02" }),
+    ]);
+  });
+
+  it("Should  be able to fetch paginated check-in history", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: "user-01",
+      });
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: "user-01",
+      page: 2,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: "gym-21" }),
+      expect.objectContaining({ gym_id: "gym-22" }),
+    ]);
+  });
+});
