f5c364b021ca067bd3e5577f47ff9e87ce1ee5a9
André Luiz <silva.ala82@gmail.com>
2024-01-03 23:19:35 -0300

    feat: implements authenticate controller and session route

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { AuthenticateUseCase } from "@/use-cases/authenticate";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+
+export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(req.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message });
+    }
+
+    throw error;
+  }
+
+  return reply.status(200).send();
+}
