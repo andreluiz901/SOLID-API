2e83f7f2b08742e9fa34fbff7e51e93116ccf5d3
André Luiz <silva.ala82@gmail.com>
2024-01-05 14:39:05 -0300

    chore: adjusts configs for E2E tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      [
+        "src/http/controllers/**",
+        "/prisma/vitest-enviroments-prisma/prisma-test-environment",
+      ],
+    ],
+    dir: "src",
+  },
 });
