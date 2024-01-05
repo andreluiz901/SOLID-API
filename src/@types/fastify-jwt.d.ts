8c4ca5aab5a7945ffbcd98d994ef50bd3966a30d
André Luiz <silva.ala82@gmail.com>
2024-01-05 11:09:48 -0300

    implements jwt validation function at profile controller as a middleware

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -4,7 +4,7 @@ declare module "@fastify/jwt" {
   export interface FastifyJWT {
     payload?: { id: number };
     user: {
-      sub: string;
+      id: string;
     };
   }
 }
--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,10 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function profile(req: FastifyRequest, reply: FastifyReply) {
-  await req.jwtVerify();
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  req.user;
+  const { user } = await getUserProfile.execute({ userId: req.user.id });
 
-  return reply.status(200).send({ a: req.user });
+  return reply.status(200).send({ ...user, password_hash: undefined });
 }
