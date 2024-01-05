f064ab7ddf9b70764f9657d94158cad284234a93
André Luiz <silva.ala82@gmail.com>
2024-01-05 16:07:01 -0300

    test: ensures that should be able authenticate by e2e test

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,30 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("", () => {});
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    app.ready();
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "valid_name",
+      email: "example@email.com",
+      password: "valid_password@123456",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "example@email.com",
+      password: "valid_password@123456",
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
