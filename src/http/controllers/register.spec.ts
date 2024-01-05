79b1cff5d3ab4a9207ebeede47acdfc8e614560b
André Luiz <silva.ala82@gmail.com>
2024-01-05 15:42:45 -0300

    test: ensures that should be able to register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("", () => {});
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    app.ready();
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "valid_name",
+      email: "example@email.com",
+      password: "valid_password@123456",
+    });
+    expect(response.statusCode).toBe(201);
+  });
+});
