f92218d829cf31e971665bdd0583c07603eb63d0
André Luiz <silva.ala82@gmail.com>
2024-01-05 08:34:15 -0300

    feat: implements prisma user and check ins repository

---
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -0,0 +1,64 @@
+import { CheckIn, Prisma } from "@prisma/client";
+import { CheckInsRepository } from "../check-ins-repository";
+import { prisma } from "@/lib/prisma";
+import dayjs from "dayjs";
+
+export class PrismaCheckInsRepository implements CheckInsRepository {
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = await prisma.checkIn.create({
+      data,
+    });
+
+    return checkIn;
+  }
+
+  async save(dataCheckIn: CheckIn) {
+    const checkIn = await prisma.checkIn.update({
+      where: { id: dataCheckIn.id },
+      data: dataCheckIn,
+    });
+
+    return checkIn;
+  }
+
+  async findById(id: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: { id },
+    });
+
+    return checkIn;
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
+
+    const checkIn = await prisma.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfTheDay.toDate(),
+          lte: endOfTheDay.toDate(),
+        },
+      },
+    });
+
+    return checkIn;
+  }
+
+  async findManyByuserId(userId: string, page: number) {
+    const checkIns = await prisma.checkIn.findMany({
+      where: { id: userId },
+      skip: (page - 1) * 20,
+      take: 20,
+    });
+
+    return checkIns;
+  }
+
+  async countByUserId(userId: string) {
+    const count = await prisma.checkIn.count({ where: { user_id: userId } });
+
+    return count;
+  }
+}
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,16 @@ import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
@@ -12,6 +22,7 @@ export class PrismaUsersRepository implements UsersRepository {
 
     return user;
   }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({ data });
 
