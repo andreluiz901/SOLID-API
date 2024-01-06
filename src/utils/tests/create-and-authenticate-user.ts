7c8463f837a27d82165430904b589a21e8586c12
André Luiz <silva.ala82@gmail.com>
2024-01-05 23:36:44 -0300

    test: ensures that should be able to use RBAC at RBAC routes

--- src/utils/tests/create-and-authenticate-user.ts
+++ src/utils/tests/create-and-authenticate-user.ts
@@ -1,11 +1,20 @@
 import request from "supertest";
 import { FastifyInstance } from "fastify";
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+import { Role } from "@prisma/client";
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post("/users").send({
-    name: "valid_name",
-    email: "example@email.com",
-    password: "valid_password@123456",
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  role?: Role
+) {
+  const user = await prisma.user.create({
+    data: {
+      name: "valid_name",
+      email: "example@email.com",
+      password_hash: await hash("valid_password@123456", 6),
+      role: role,
+    },
   });
 
   const authResponse = await request(app.server).post("/sessions").send({
